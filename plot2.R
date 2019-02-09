264953dd44862f7e0ae97cce530d1c1fa8e2092b
Jason Seril <jasonseril@gmail.com>
2019-02-09 23:24:29 +0800

    added some parameters

--- plot2.R
+++ plot2.R
@@ -7,12 +7,16 @@ library(ggplot2)
 ## Loading the data
 hpcdata <- read.csv("household_power_consumption.txt", header=T, sep=";",
                     na.strings="?", check.names=F, stringsAsFactors=F,
-                    nrows=2075259, comment.char="", quote='\"')
+                     comment.char="", quote='\"')
 powerdata <- subset(hpcdata, Date %in% c("1/2/2007","2/2/2007"))
 powerdata$Date <- as.Date(powerdata$Date, format="%d/%m/%Y")
+datetime <- paste(as.Date(powerdata$Date), powerdata$Time)
+powerdata$Datetime <- as.POSIXct(datetime)
 
-## Plot 1
-png(filename = "plot1.png", width=480,height=480)
-hist(powerdata$Global_active_power, main="Global Active Power",
-     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="red")
+## Plot 2
+with(powerdata, {
+        plot(Global_active_power~Datetime, type="l",
+             ylab="Global Active Power (kilowatts)", xlab="")
+})
+png("plot2.png", width=480,height=480)
 dev.off()
