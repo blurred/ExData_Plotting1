ccd5e7bc88b9d3fa81359ffcd382c8a4e8bec35d
Jason Seril <jasonseril@gmail.com>
2019-02-09 23:26:39 +0800

    transferred png function before the plot

--- plot2.R
+++ plot2.R
@@ -7,16 +7,17 @@ library(ggplot2)
 ## Loading the data
 hpcdata <- read.csv("household_power_consumption.txt", header=T, sep=";",
                     na.strings="?", check.names=F, stringsAsFactors=F,
-                     comment.char="", quote='\"')
+                    nrows=2075259,comment.char="", quote='\"')
 powerdata <- subset(hpcdata, Date %in% c("1/2/2007","2/2/2007"))
 powerdata$Date <- as.Date(powerdata$Date, format="%d/%m/%Y")
 datetime <- paste(as.Date(powerdata$Date), powerdata$Time)
 powerdata$Datetime <- as.POSIXct(datetime)
 
 ## Plot 2
+png("plot2.png", width=480,height=480)
 with(powerdata, {
         plot(Global_active_power~Datetime, type="l",
              ylab="Global Active Power (kilowatts)", xlab="")
 })
-png("plot2.png", width=480,height=480)
+
 dev.off()
