e3aaa0467adc20c71c85d1f1fab70dc84887628e
Jason Seril <jasonseril@gmail.com>
2019-02-09 23:47:05 +0800

    initial commit

---
+++ plot4.R
@@ -0,0 +1,25 @@
+## Set working directory
+setwd("C:\\Users\\jason\\Documents\\EDUCATION\\COURSERA\\JHU DATA SCIENCE SPECIALIZATION\\EXPLORATORY DATA ANALYSIS\\WEEK1\\ExData_Plotting1")
+
+## 
+library(ggplot2)
+
+## Loading the data
+hpcdata <- read.csv("household_power_consumption.txt", header=T, sep=";",
+                    na.strings="?", check.names=F, stringsAsFactors=F,
+                    nrows=2075259,comment.char="", quote='\"')
+powerdata <- subset(hpcdata, Date %in% c("1/2/2007","2/2/2007"))
+powerdata$Date <- as.Date(powerdata$Date, format="%d/%m/%Y")
+datetime <- paste(as.Date(powerdata$Date), powerdata$Time)
+powerdata$Datetime <- as.POSIXct(datetime)
+
+## Plot 4
+png("plot3.png", width=480,height=480)
+par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
+with(powerdata, {
+        plot(Global_active_power~Datetime, type="1", 
+             ylab="Global Active Power (kilowatts)", xlab="")
+        plot(Voltage~Datetime, type="1", 
+             ylab="Voltage (volt)", xlab="")
+})
+dev.off()
