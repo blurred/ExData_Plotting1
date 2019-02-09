0ebfe1bbf53a67b74ca20e891964398c60669482
Jason Seril <jasonseril@gmail.com>
2019-02-09 23:38:23 +0800

    added col parameter in lines function

--- plot3.R
+++ plot3.R
@@ -18,8 +18,8 @@ png("plot3.png", width=480,height=480)
 with(powerdata, {
         plot(Sub_metering_1~Datetime, type="l",
              ylab="Global Active Power (kilowatts)", xlab="")
-        lines(Sub_metering_2~Datetime)
-        lines(Sub_metering_3~Datetime)
+        lines(Sub_metering_2~Datetime, col="Red")
+        lines(Sub_metering_3~Datetime, col="Blue")
 })
 legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
        legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
